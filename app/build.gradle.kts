6875d2d2fbb5be821ba950d6af384e6b0da54ed3
Vauchi CI <ci@vauchi.com>
2026-02-26 18:17:06 +0000

    chore: Bump vauchi-mobile to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,7 +141,7 @@ dependencies {
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
 
     // VauchiMobile native bindings (includes JNA + JNI libs)
-    implementation("com.vauchi:vauchi-mobile:0.1.21")
+    implementation("com.vauchi:vauchi-mobile:0.2.0")
 
     // Testing
     testImplementation(kotlin("test"))
