01c65dafb1124b95e83709205dfd70708639a540
project-access-token <group_122552022_bot_c9ed9ee134fc5d1058cebb4e160abc23@noreply.gitlab.com>
2026-03-27 11:32:27 +0000

    chore: Bump vauchi-platform to 0.10.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -190,7 +190,7 @@ dependencies {
         implementation("net.java.dev.jna:jna:5.14.0@aar")
     } else {
         // Remote: published AAR includes JNA + JNI libs + Kotlin bindings
-        implementation("app.vauchi:vauchi-platform:0.9.0")
+        implementation("app.vauchi:vauchi-platform:0.10.6")
     }
 
     // Testing
