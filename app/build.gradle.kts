6955cc87252574a90e122ef0d38f11d606f68d6e
project-access-token <group_122552022_bot_c9ed9ee134fc5d1058cebb4e160abc23@noreply.gitlab.com>
2026-05-28 07:51:58 +0000

    chore: Bump vauchi-platform to 0.51.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -205,7 +205,7 @@ dependencies {
         implementation("net.java.dev.jna:jna:5.14.0@aar")
     } else {
         // Remote: published AAR includes JNA + JNI libs + Kotlin bindings
-        implementation("app.vauchi:vauchi-platform:0.51.17")
+        implementation("app.vauchi:vauchi-platform:0.51.18")
     }
 
     // Testing
@@ -232,7 +232,7 @@ dependencies {
     if (useLocalBindings) {
         screenshotTestImplementation("net.java.dev.jna:jna:5.14.0@aar")
     } else {
-        screenshotTestImplementation("app.vauchi:vauchi-platform:0.51.17")
+        screenshotTestImplementation("app.vauchi:vauchi-platform:0.51.18")
     }
     screenshotTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     screenshotTestImplementation("androidx.compose.ui:ui")
